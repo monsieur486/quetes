2b7959c95aa9337925fd30ddd11ed57573e4c1f4
Monsieur486 <monsieur486@gmail.com>
2019-12-05 17:03:20 +0100

    first commit

--- go.sh
+++ go.sh
@@ -2,6 +2,10 @@
 source branche.sh
 php composer.phar install
 yarn install
+php bin/console translation:update --dump-messages fr --domain=messages
+php bin/console translation:update --dump-messages en --domain=messages
+php bin/console translation:update --force fr --domain=messages
+php bin/console translation:update --force en --domain=messages
 yarn encore dev
 git add .
 echo "Message commit :" $msg_branche
