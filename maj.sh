76664d7f63388133d161b9be0fc681d1d53e828d
Monsieur486 <monsieur486@gmail.com>
2019-12-11 11:34:43 +0100

    first commit

--- maj.sh
+++ maj.sh
@@ -10,8 +10,9 @@ php bin/console translation:update --force en --domain=messages
 php bin/console translation:update --force de --domain=messages
 php bin/console cache:clear
 yarn encore dev
-git add .
-git commit -m "first commit"
-php bin/console doctrine:schema:update --force
+php bin/console make:migration
+php bin/console doctrine:migrations:migrate
 php bin/console doctrine:mapping:info
 php bin/console doctrine:schema:validate
+git add .
+git commit -m "first commit"
